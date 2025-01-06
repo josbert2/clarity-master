05c63205c22b3b14e376da4779c97b310b88ae44
josbert2 <hernandezjosbert@gmail.com>
2025-01-06 17:55:36 -0300

    UI: fix tienda online

--- apps/web-app/src/App.tsx
+++ apps/web-app/src/App.tsx
@@ -1,13 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import { isBlank } from 'common'
+
+import { IStaticMethods } from 'flyonui/flyonui';
+
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  console.log(IStaticMethods)
   return (
     <>
       <p>undefined isBlank - {isBlank(undefined) ? 'true' : 'false'}</p>
---
+++ apps/web-app/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './node_modules/flyonui/dist/js/*.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+
+    require('clarity'),
+
+  ],
+}
+
